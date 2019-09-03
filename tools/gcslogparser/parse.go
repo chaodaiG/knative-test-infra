89c6e62a94e2253ae07ababaa5dd01406ddbd352
Chao Dai <chaodai@google.com>
2019-09-03 09:18:31 -0700

    better formatting output

--- tools/gcslogparser/parse.go
+++ tools/gcslogparser/parse.go
@@ -51,22 +51,22 @@ func parseOptions() *Flags {
 	flag.StringVar(&f.jobFilter, "jobs", "", "jobs to be analyzed, comma separated")
 	flag.BoolVar(&f.prOnly, "pr-only", false, "supplied if just want to analyze PR jobs")
 	flag.BoolVar(&f.ciOnly, "ci-only", false, "supplied if just want to analyze CI jobs")
-	flag.StringVar(&f.groupBy, "groupby", "job(default)", "output groupby, supports: match(group by matches)")
+	flag.StringVar(&f.groupBy, "groupby", "job(default)", "output groupby, supports: match(group by matches), repo(group by repo")
 	flag.StringVar(&f.runnerHost, "host", "gcslogparser-runner-image.default.example.com", "host name of runner service)")
 	flag.StringVar(&f.runnerIP, "ip", "34.68.162.48", "ip address of runner service")
 	flag.Parse()
 	return &f
 }
 
-func groupByJob(found [][]string) {
-	var msgs []string
-	for _, elems := range found {
-		msgs = append(msgs, strings.Join(elems, ","))
-	}
-	log.Printf("\n\n%s", strings.Join(msgs, "\n"))
+func groupByJob(found [][]string) string {
+	sort.Slice(found, func(i, j int) bool {
+		return found[i][1] < found[j][1]
+	})
+	return printMsg(found)
 }
 
-func groupByMatch(found [][]string) {
+func groupByMatch(found [][]string) string {
+	msg := ""
 	outArr := make(map[string][][]string)
 	for _, l := range found {
 		if _, ok := outArr[l[0]]; !ok {
@@ -78,12 +78,61 @@ func groupByMatch(found [][]string) {
 		sort.Slice(sl, func(i, j int) bool {
 			return sl[i][1] > sl[j][1]
 		})
-		var msgs []string
-		for _, elems := range sl {
-			msgs = append(msgs, strings.Join(elems, ","))
-		}
-		log.Printf("\n\n%s", strings.Join(msgs, "\n"))
+		msg += printMsg(sl)
 	}
+	return msg
+}
+
+func groupByRepo(found [][]string) string {
+	sort.Slice(found, func(i, j int) bool {
+		if getJobName(found[i][2]) != getJobName(found[j][2]) {
+			return getJobName(found[i][2]) < getJobName(found[j][2])
+		}
+		return found[i][1] < found[j][1]
+	})
+	return printMsg(found)
+}
+
+func printMsg(found [][]string) string {
+	var releaseJobs [][]string
+	var presubmitJobs [][]string
+	var nightlyJobs [][]string
+	var otherJobs [][]string
+	for _, elems := range found {
+		jobName := getJobName(elems[2])
+		if strings.Contains(jobName, "release") {
+			releaseJobs = append(releaseJobs, elems)
+		} else if strings.HasPrefix(jobName, "pull") {
+			presubmitJobs = append(presubmitJobs, elems)
+		} else if strings.HasPrefix(jobName, "ci") {
+			nightlyJobs = append(nightlyJobs, elems)
+		} else {
+			otherJobs = append(otherJobs, elems)
+		}
+	}
+
+	msg := fmt.Sprintf("\n\t\tRelease jobs: %d\n\t\tPresubmit jobs: %d\n\t\tNightly jobs: %d\n\t\tOther jobs: %d",
+		len(releaseJobs), len(presubmitJobs), len(nightlyJobs), len(otherJobs))
+
+	return fmt.Sprintf("%s\n\n%s\n%s\n%s", msg, msgForType(releaseJobs, "Release Jobs"), msgForType(presubmitJobs, "Presubmit jobs"),
+		msgForType(otherJobs, "Other Jobs"))
+}
+
+func msgForType(found [][]string, name string) string {
+	msg := fmt.Sprintf("%s(%d):", name, len(found))
+	for _, elems := range found {
+		msg = fmt.Sprintf("%s\n\t%s", msg, strings.Join(elems[1:], ","))
+	}
+	return msg
+}
+
+func getJobName(jobURL string) string {
+	for _, part := range strings.Split(jobURL, "/") {
+		if strings.Contains(part, "-knative-") {
+			return part
+		}
+	}
+	return ""
 }
 
 func parse(f *Flags) *Parser {
@@ -132,26 +181,33 @@ func main() {
 	}
 
 	c := parse(f)
-	switch f.groupBy {
-	case "job":
-		groupByJob(c.found)
-	case "match":
-		groupByMatch(c.found)
-	default:
-		log.Printf("--groupby doesn't support %s, fallback to default", f.groupBy)
-		groupByJob(c.found)
-	}
+
 	summary := fmt.Sprintf("Summary:\nQuerying jobs from repos: '%s'", f.repoNames)
-	summary = fmt.Sprintf("%s\nQuerying pattern: '%s'", summary, f.parseRegex)
+	summary = fmt.Sprintf("%s\nQuerying pattern: '%s'", summary, f.parse+f.parseRegex)
 	if "" != f.startDate {
 		summary = fmt.Sprintf("%s\nStart date: %s", summary, f.startDate)
 	}
 	if "" != f.endDate {
 		summary = fmt.Sprintf("%s\nEnd date: %s", summary, f.endDate)
 	}
-	summary = fmt.Sprintf("%s\nResults:\n\tProcessed jobs: %d\n\tFound matches: %d\n\n\n\n\nNo log found: %d",
-		summary, len(c.processed), len(c.found), c.failedCount)
+	summary = fmt.Sprintf("%s\nResults:\n\tProcessed jobs: %d\n\tFound matches: %d",
+		summary, len(c.processed), len(c.found))
 
-	log.Print(summary)
+	var ind string
+	switch f.groupBy {
+	case "job":
+		ind = groupByJob(c.found)
+	case "match":
+		ind = groupByMatch(c.found)
+	case "repo":
+		ind = groupByRepo(c.found)
+	default:
+		// log.Printf("--groupby doesn't support %s, fallback to default", f.groupBy)
+		ind = groupByJob(c.found)
+	}
 
+	summary = fmt.Sprintf("%s%s", summary, ind)
+	log.Println(summary)
+
+	log.Printf("\n\n\n\n\nNo log found: %d", c.failedCount)
 }
